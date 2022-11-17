4d090ddb1284da8dcc5c62dddda97b31eb4fca26
Francesco Bognini <info@francescobognini.com>
2022-11-17 15:17:26 +0100

    net7 compatibility

--- src/fbognini.WebFramework/OpenApi/SwaggerHeaderAttribute.cs
+++ src/fbognini.WebFramework/OpenApi/SwaggerHeaderAttribute.cs
@@ -10,7 +10,7 @@ public class SwaggerHeaderAttribute : Attribute
     public string DefaultValue { get; }
     public bool IsRequired { get; }
 
-    public SwaggerHeaderAttribute(string headerName, string? description = null, string? defaultValue = null, bool isRequired = false)
+    public SwaggerHeaderAttribute(string headerName, string description = null, string defaultValue = null, bool isRequired = false)
     {
         HeaderName = headerName;
         Description = description;
