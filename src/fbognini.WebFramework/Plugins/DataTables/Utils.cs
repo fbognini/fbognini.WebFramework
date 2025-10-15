5437424ae932cc24b6609a33e1c98e979592c290
Francesco Bognini <info@francescobognini.com>
2025-10-15 18:00:32 +0200

    Update fbognini dependencies

--- src/fbognini.WebFramework/Plugins/DataTables/Utils.cs
+++ src/fbognini.WebFramework/Plugins/DataTables/Utils.cs
@@ -7,29 +7,25 @@ namespace fbognini.WebFramework.Plugins.DataTables
 {
     public static class Utils
     {
-        public static void LoadDtParameters(DtParameters dtParameters, out List<SortingQuery> sortings, out PaginationOffsetQuery pagination)
+        public static void LoadDtParameters(DtParameters dtParameters, out Dictionary<string, SortingDirection> sortings, out PaginationOffsetQuery pagination)
         {
             if (dtParameters == null || dtParameters.Columns == null)
                 throw new ArgumentNullException(nameof(dtParameters));
 
-            sortings = new List<SortingQuery>();
+            sortings = new Dictionary<string, SortingDirection>();
 
             if (dtParameters.Order != null)
             {
                 for (int i = 0; i < dtParameters.Order.Length; i++)
                 {
-                    sortings.Add(
-                        new SortingQuery(
-                            dtParameters.Columns[dtParameters.Order[i].Column].Data, 
-                            dtParameters.Order[i].Dir == DtOrderDir.ASC 
-                                ? SortingDirection.ASCENDING 
-                                : SortingDirection.DESCENDING));
-
+                    sortings.Add(dtParameters.Columns[dtParameters.Order[i].Column].Data, dtParameters.Order[i].Dir == DtOrderDir.ASC
+                                ? SortingDirection.ASCENDING
+                                : SortingDirection.DESCENDING);
                 }
             }
             else
             {
-                sortings.Add(new SortingQuery(dtParameters.Columns[0].Data, SortingDirection.ASCENDING));
+                sortings.Add(dtParameters.Columns[0].Data, SortingDirection.ASCENDING);
             }
 
             pagination = new PaginationOffsetQuery(dtParameters.Length, dtParameters.Start / dtParameters.Length + 1);
